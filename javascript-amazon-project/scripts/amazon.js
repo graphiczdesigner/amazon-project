f4b7b50b672030b887867b1dd730d3a19ff4af17
Zeeshan Shahzad <zeeshanshahzad787@gmail.com>
2025-09-02 10:09:56 -0700

    input products from products.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,5 @@
 import {cart} from '../data/cart.js';
+import {products} from '../data/products.js'
 // Add products in html using js
 let productsHTML = '';
 // products is a variable name oof an array from data/products.js
