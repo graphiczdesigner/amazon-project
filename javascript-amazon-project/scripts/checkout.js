73ba8327de1ed7062f0c7c8c8a26a26a502892ac
Zeeshan Shahzad <zeeshanshahzad787@gmail.com>
2025-09-11 07:17:13 -0700

    Remove Cart Item from page when click on delete button

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
         }
     });
     cartSummaryHTML +=`
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,5 +101,8 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', ()=>{
             const productId = link.dataset.productId;
             removeFromCart(productId);
+            const container = document.querySelector(
+                `.cart-item-container-${productId}`);
+                container.remove();
         });
     });
